c3b0741fc74b822707dee7d154bd033668e56e98
Eugene Pankov <john.pankov@gmail.com>
2021-07-07 01:00:40 +0200

    wip

--- tabby/asgi.py
+++ tabby/asgi.py
@@ -1,10 +1,12 @@
+import django
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabby.settings')
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabby.settings')
 
 from .app.urls import websocket_urlpatterns
 
