cf18f507efc36edc1e1aa5966ea8adc2189b5c47
Eugene Pankov <e@ajenti.org>
2022-05-16 22:38:01 -0700

    set SESSION_COOKIE_SAMESITE

--- backend/tabby/settings.py
+++ backend/tabby/settings.py
@@ -240,6 +240,7 @@ if FRONTEND_URL:
     if BACKEND_URL:
         CSRF_TRUSTED_ORIGINS.append(urlparse(BACKEND_URL).hostname)
     SESSION_COOKIE_DOMAIN = frontend_domain
+    SESSION_COOKIE_SAMESITE = None
     CSRF_COOKIE_DOMAIN = frontend_domain
 
     FRONTEND_URL = FRONTEND_URL.rstrip('/')
