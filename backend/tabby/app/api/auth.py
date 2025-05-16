773386c286c3a205b230e2507152962e797e4d8f
Ibaraki <ibaraki@ibaraki.app>
2025-05-16 18:30:56 +0200

    fix: backend linter

--- backend/tabby/app/api/auth.py
+++ backend/tabby/app/api/auth.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from ..models import User
 from .user import UserSerializer
 
 
